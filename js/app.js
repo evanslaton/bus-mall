a6a371d6fc364f9e08c9eff4c2db5bcf956c7dd0
Evan Slaton <evanslaton@gmail.com>
2018-08-20 16:13:49 -0700

    Resized images and can display any number of images

--- js/app.js
+++ js/app.js
@@ -6,13 +6,25 @@ var productObjectsArray = [];
 function Product(name) {
   this.name = name;
   this.path = `img/${name}.jpg`;
-  this.timesClick = 0;
+  this.timesClicked = 0;
   this.timesShown = 0;
   productObjectsArray.push(this);
 }
 
-var productNameArray = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+// Determines how many numbers to display
+/* ============================ */
+Product.numOfProductsToDisplay = 3;
+/* ============================ */
 
+Product.ulEl = document.getElementById('product-images');
+Product.previousProductsShown = [];
+Product.currentProductsShown = [];
+
+Product.getRandomNum = function() {
+  return Math.floor(Math.random() * productNameArray.length);
+};
+
+var productNameArray = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 // IIFE that creates a new object for each product using the constructor function
 (function() {
@@ -21,4 +33,44 @@ var productNameArray = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubb
   });
 })();
 
-console.log(productObjectsArray);
+// Generates unique random numbers that are different than the previous unique random numbers
+var currentRandomNums = function() {
+  var randomNum;
+  Product.previousProductsShown = Product.currentProductsShown;
+  Product.currentProductsShown = [];
+
+  while (Product.currentProductsShown.length < Product.numOfProductsToDisplay) {
+    randomNum = Product.getRandomNum();
+    if (Product.currentProductsShown.indexOf(randomNum) === -1 && Product.previousProductsShown.indexOf(randomNum) === -1) {
+      Product.currentProductsShown.push(randomNum);
+    }
+  }
+  console.log('Previous', Product.previousProductsShown);
+  console.log('Current', Product.currentProductsShown);
+  console.log('***********************************************');
+};
+
+currentRandomNums();
+
+// Renders products to the screen
+var renderProducts = function() {
+
+  for (var i = 0; i < Product.numOfProductsToDisplay; i++) {
+    var ilEl = document.createElement('li');
+    var figureEl = document.createElement('figure');
+    var imgEl = document.createElement('img');
+    imgEl.src = productObjectsArray[Product.currentProductsShown[i]].path;
+    imgEl.alt = productObjectsArray[Product.currentProductsShown[i]].name;
+    var figCaptionEl = document.createElement('figcaption');
+    figCaptionEl.textContent = productObjectsArray[Product.currentProductsShown[i]].name;
+
+    Product.ulEl.appendChild(ilEl);
+    ilEl.appendChild(figureEl);
+    figureEl.appendChild(imgEl);
+    figureEl.appendChild(figCaptionEl);
+  }
+};
+
+renderProducts();
+
+
